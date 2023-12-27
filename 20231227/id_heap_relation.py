20984e3eb5538eb8ff3516a845dd6631a85d50c1
KMS <kms01983@hotmail.com>
2023-12-27 13:54:01 +0900

    1:53pm save

---
+++ 20231227/id_heap_relation.py
@@ -0,0 +1,13 @@
+year = 2024
+print(id(2024))
+print(id(year))
+# 위 예시는 같은 주소를 가지는 것을 확인할 수 있음
+
+year = 2023
+print(id(year))
+# year 변수의 값이 바뀔 경우 주소가 변경된다.
+
+
+year = '2023'
+print(id(year))
+# 위 경우 문자열 2023으로 새로 저장된다.
